4fc0aa914a064faf58143e95c863ffe3a49059e6
Antony Budianto <antonybudianto@gmail.com>
2023-03-28 11:55:24 +0700

    impl new ui

--- app/components/HomeClient.tsx
+++ app/components/HomeClient.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { encode } from "@nem035/gpt-3-encoder";
 import { useCallback, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
@@ -8,6 +9,8 @@ interface Chat {
   user: string;
   prompt: string;
 }
+
+const MAX_TOKEN = 4096;
 
 export default function HomeClient() {
   const [prompt, setPrompt] = useState("");
@@ -28,20 +31,39 @@ export default function HomeClient() {
      * To give context to the API
      */
     const assistances = chats
-      .filter((_chat) => _chat.user === "bot")
+      /**
+       * @TODO not sure to include user chats as well to the payload...
+       */
+      // .filter((_chat) => _chat.user === "bot")
       .map((_chat) => {
         return {
-          role: "assistant",
+          role: _chat.user === "bot" ? "assistant" : "user",
           content: _chat.prompt,
         };
       });
-    const payload = [
+    let payload = [
       ...assistances,
       {
         role: "user",
         content: promptText,
       },
     ];
+
+    let tokenOk = false;
+    let finalPayload = [];
+
+    let tokenCount = 0;
+    for (let i = payload.length - 1; i >= 0; i--) {
+      tokenCount += encode(payload[i].content).length;
+      tokenOk = tokenCount < MAX_TOKEN;
+      if (tokenOk) {
+        finalPayload.push(payload[i]);
+      } else {
+        console.warn("Token limit reached: ", tokenCount);
+        break;
+      }
+    }
+    finalPayload.reverse();
 
     try {
       const response = await fetch("/api/gpt", {
@@ -50,7 +72,7 @@ export default function HomeClient() {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          prompt: payload,
+          prompt: finalPayload,
         }),
       });
 
@@ -81,6 +103,7 @@ export default function HomeClient() {
           lastChat.prompt += chunkValue;
           return [..._chats.slice(0, _chats.length - 1), lastChat];
         });
+        window.scrollTo(0, document.body.scrollHeight);
       }
     } catch (e) {
       console.error(e);
@@ -93,37 +116,27 @@ export default function HomeClient() {
     setPrompt(e.target.value);
   }, []);
 
-  const handleSubmit = useCallback(() => {
-    generate(prompt);
-  }, [prompt]);
+  const handleSubmit = useCallback(
+    (e: any) => {
+      e.preventDefault();
+      generate(prompt);
+    },
+    [prompt]
+  );
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col mb-16">
       <h1 className="font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-cyan-400 to-green-600">
         ChatGPT
       </h1>
-      <textarea
-        className="px-3 py-2 mt-5"
-        placeholder="Input your prompt"
-        onChange={handleChange}
-        value={prompt}
-        disabled={loading}
-      ></textarea>
-      <button
-        type="button"
-        className="px-2 py-3 bg-gray-900 mt-3 disabled:bg-gray-600"
-        onClick={handleSubmit}
-        disabled={loading}
-      >
-        Send
-      </button>
-      <div className="my-10">
+
+      <div className="my-5">
         {chats.map((chat, i) => (
           <div
-            className={`px-3 py-2 mt-3 rounded ${
+            className={`px-3 py-2 rounded ${
               chat.user === "bot"
                 ? "bg-gray-800 text-gray-300 mt-0"
-                : "bg-gray-900"
+                : "bg-gray-900 mt-3"
             }`}
             key={i}
           >
@@ -131,6 +144,24 @@ export default function HomeClient() {
           </div>
         ))}
       </div>
+      <form noValidate onSubmit={handleSubmit}>
+        <div className="flex justify-center align-center fixed mb-10 bottom-2 left-0 right-0">
+          <textarea
+            className="px-4 py-3 h-12 max-h-12 w-2/4 rounded"
+            placeholder="Input your prompt"
+            onChange={handleChange}
+            value={prompt}
+            disabled={loading}
+          ></textarea>
+          <button
+            type="submit"
+            className="px-5 py-0 bg-gray-800 font-bold rounded text-2xl hover:bg-gray-700 disabled:bg-gray-600"
+            disabled={loading}
+          >
+            {">"}
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
