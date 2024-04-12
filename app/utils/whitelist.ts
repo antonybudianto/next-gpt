a32cc89a8cb8e238060e65f031bc3b9e68135cbe
antony <antonybudianto@gmail.com>
2024-04-12 15:52:43 +0700

    fix email

--- app/utils/whitelist.ts
+++ app/utils/whitelist.ts
@@ -1,4 +1,4 @@
-const WHITELIST_EMAIL = ["antonybudianto@gmail.com"];
+const WHITELIST_EMAIL = ["antonybudianto@gmail.com", "huangg.helen@gmail.com"];
 
 export const isWhitelisted = (email: string) =>
   WHITELIST_EMAIL.some((em) => em === email);
